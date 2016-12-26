c4a0285b6ed0210492c303be31ae0f8525289b8e
blupie01 <jeffrey.chou.89@gmail.com>
2016-12-26 14:49:47 -0500

    added css/bootstrap

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyB8hJRJeScaWopXWCzPEgG7UA0VnG5Vo0E",
+	authDomain: "trainspotter-45428.firebaseapp.com",
+	databaseURL: "https://trainspotter-45428.firebaseio.com",
+	storageBucket: "trainspotter-45428.appspot.com",
+	messagingSenderId: "642093108830"
+};
+firebase.initializeApp(config);
+
+
