ac31f42240d9b3508fd2c2ccff40d0003bc552c9
blupie01 <jeffrey.chou.89@gmail.com>
2016-12-26 18:29:15 -0500

    12.26.16 push 2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,4 +8,40 @@ var config = {
 };
 firebase.initializeApp(config);
 
+//var to hold database info
+var trainDataBase = firebase.database();
 
+//var to hold information from form
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var trainFrequency = "";
+
+$(document).ready(function() {
+	//var to hold train-schedule div
+	var currentTrainSchedule = $("#train-schedule");
+	//var to hold form information
+	var form = $("#train-adder-form");
+
+	form.submit(function(e){
+		//var to hold info entered by user
+		trainName = $("#train-name").val();
+		destination = $("#train-destination").val();
+		firstTrainTime = $("#first-train-time").val();
+		trainFrequency = $("#train-frequency").val();
+		console.log(trainName, destination, firstTrainTime, trainFrequency);
+
+		//var to hold a row
+		var row = $("<div class=row>");
+		//var to hold a col for trainName
+		var trainNameCol = $("<div class=col-md-4>").text(trainName);
+		//var to hold a col for destincation
+		var destinationCol = $("<div class=col-md-4>").text(destination);
+		//var to hold a col for frequency
+		var trainFrequencyCol = $("<div class=col-md-4>").text(trainFrequency);
+
+		completedRow = row.append(trainNameCol).append(destinationCol).append(trainFrequencyCol);
+		currentTrainSchedule.append(row);
+		e.preventDefault();
+	})
+})
