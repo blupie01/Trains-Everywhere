cc4589399583d02b5ccd6ba606308be729508803
Jeff <Jeff>
2016-12-28 19:54:43 -0500

    it works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,24 +24,100 @@ $(document).ready(function() {
 	var form = $("#train-adder-form");
 
 	form.submit(function(e){
+		e.preventDefault();
 		//var to hold info entered by user
-		trainName = $("#train-name").val();
-		destination = $("#train-destination").val();
-		firstTrainTime = $("#first-train-time").val();
-		trainFrequency = $("#train-frequency").val();
+		trainName = $("#train-name").val().trim();
+		destination = $("#train-destination").val().trim();
+		firstTrainTime = $("#first-train-time").val().trim();
+		trainFrequency = $("#train-frequency").val().trim();
 		console.log(trainName, destination, firstTrainTime, trainFrequency);
+
+		// Creates local "temporary" object for holding employee data
+ 		var newTrain = {
+		   name: trainName,
+		   dest: destination,
+		   firstTime: firstTrainTime,
+		   freq: trainFrequency
+		 };
+
+		// Uploads train data to the database
+		trainDataBase.ref().push(newTrain);
+
+		// Logs everything to console
+		console.log(newTrain.name);
+		console.log(newTrain.dest);
+		console.log(newTrain.firstTime);
+		console.log(newTrain.freq);
+
+		// Alert
+		alert("train successfully added");
+
+		 // Clears all of the text-boxes
+		$("#train-name").val("");
+		$("#train-destination").val("");
+		$("#first-train-time").val("");
+		$("#train-frequency").val("");
+
+		 // Prevents moving to new page
+		return false;
+		});
+
+		//pull from database and prints to screen/html
+		trainDataBase.ref().on("child_added", function(childSnapshot) {
+
+		console.log(childSnapshot.val());
+
+		// Store everything into a variable.
+		var trainName = childSnapshot.val().name;
+		var destination = childSnapshot.val().dest;
+		var firstTrainTime = childSnapshot.val().firstTime;
+		var trainFrequency = childSnapshot.val().freq;
+
+		// Employee Info
+		console.log(trainName);
+		console.log(destination);
+		console.log(firstTrainTime);
+		console.log(trainFrequency);
+
+		// First Time (pushed back 1 year to make sure it comes before current time)
+	    var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+	    console.log(firstTimeConverted);
+
+	     // Current Time
+	    var currentTime = moment();
+	    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+	    // Difference between the times
+	    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+	    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+	    // Time apart (remainder)
+	    var tRemainder = diffTime % trainFrequency;
+	    console.log(tRemainder);
+
+	    // Minute Until Train
+	    var tMinutesTillTrain = trainFrequency - tRemainder;
+	    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+	    // Next Train
+	    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+	    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 		//var to hold a row
 		var row = $("<div class=row>");
 		//var to hold a col for trainName
-		var trainNameCol = $("<div class=col-md-4>").text(trainName);
+		var trainNameCol = $("<div class=col-md-3>").text(trainName);
 		//var to hold a col for destincation
-		var destinationCol = $("<div class=col-md-4>").text(destination);
+		var destinationCol = $("<div class=col-md-3>").text(destination);
 		//var to hold a col for frequency
-		var trainFrequencyCol = $("<div class=col-md-4>").text(trainFrequency);
+		var trainFrequencyCol = $("<div class=col-md-2>").text(trainFrequency);
+		//var to hold a col for frequency
+		var tMinutesTillTrainCol = $("<div class=col-md-2>").text(tMinutesTillTrain);
+		//var to hold a col for frequency
+		var nextTrainCol = $("<div class=col-md-2>").text(moment(nextTrain).format("hh:mm A"));
 
-		completedRow = row.append(trainNameCol).append(destinationCol).append(trainFrequencyCol);
+		completedRow = row.append(trainNameCol).append(destinationCol).append(
+			trainFrequencyCol).append(nextTrainCol).append(tMinutesTillTrainCol);
 		currentTrainSchedule.append(row);
-		e.preventDefault();
 	})
 })
